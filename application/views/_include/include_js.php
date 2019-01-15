1ec27fdabda70d893c85c22c797b5309cdb31dfb
dessurya <fourline66@gmail.com>
2019-01-15 17:23:09 +0700

    Revert "Revert "Dashboard dan menu IPC""
    This reverts commit 1d18781dccfd3e104838906d21c241bf9682ae41.

--- application/views/_include/include_js.php
+++ application/views/_include/include_js.php
@@ -3,8 +3,10 @@
 <script type="text/javascript" src="<?php echo base_url().'_asset/gentelella/vendors/nprogress/nprogress.js' ?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'_asset/gentelella/js/custom.js' ?>"></script>
 
-<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.js' ?>"></script>';
-<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.buttons.js' ?>"></script>';
-<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.nonblock.js' ?>"></script>';
-<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.confirm.js' ?>"></script>';
-<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.callbacks.js' ?>"></script>';
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.buttons.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.nonblock.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.confirm.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.callbacks.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/pnotify/dist/pnotify.callbacks.js' ?>"></script>
+<script src="<?php echo base_url().'/_asset/gentelella/vendors/Chart.js/dist/Chart.min.js' ?>"></script>
